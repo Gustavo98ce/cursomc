082d5348d2efc61fef99a5f6aea941190510a8e7
Gustavo Gadelha <gustavogsousa@gmail.com>
2021-05-17 09:45:40 -0300

    Instanciação de categorias na classe main

--- src/main/java/com/gustavogadelha/cursomc/domain/Categoria.java
+++ src/main/java/com/gustavogadelha/cursomc/domain/Categoria.java
@@ -7,32 +7,30 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
-
 @Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private Integer id;
 	private String nome;
 
 	public Categoria() {
 
 	}
 
-	public Categoria(int id, String nome) {
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -44,11 +42,12 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + id;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -61,9 +60,11 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
-
 }
--- src/main/java/com/gustavogadelha/cursomc/services/CategoriaService.java
+++ src/main/java/com/gustavogadelha/cursomc/services/CategoriaService.java
@@ -3,18 +3,19 @@ package com.gustavogadelha.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.gustavogadelha.cursomc.domain.Categoria;
 import com.gustavogadelha.cursomc.repositories.CategoriaRepository;
 
+@Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria buscar(Integer id) {
+
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElse(null);
-		
 	}
 }
