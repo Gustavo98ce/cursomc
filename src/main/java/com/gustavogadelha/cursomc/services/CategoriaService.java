99ad7781bdb9b792f13a43d5ebcf051257417864
Gustavo Gadelha <gustavogsousa@gmail.com>
2021-05-25 14:15:37 -0300

    Delete method categoria

--- src/main/java/com/gustavogadelha/cursomc/services/CategoriaService.java
+++ src/main/java/com/gustavogadelha/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.gustavogadelha.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.gustavogadelha.cursomc.domain.Categoria;
 import com.gustavogadelha.cursomc.repositories.CategoriaRepository;
+import com.gustavogadelha.cursomc.services.exceptions.DataIntegrityException;
 import com.gustavogadelha.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -27,12 +29,21 @@ public class CategoriaService {
 		return repo.save(obj);
 
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
-		
+
 	}
-	
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException(" Nao e possivel excluir uma categoria que possui produtos");
+
+		}
+	}
 
 }
